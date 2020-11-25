3c019b9c616869675a2696183096e10281153495
Aleksandra Postola <59602014+aleksandra-coder@users.noreply.github.com>
2020-11-25 16:08:41 +0200

    Add files via upload
    index_2.html and finnkino_2.js updated files 25.11

---
+++ finnkino_2.js
@@ -0,0 +1,45 @@
+// fucntion is called after the user clicks the search button
+function loadDoc() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        myFunction(this);
+      }
+    };
+    // for each selected cinema from the list, get data for this cinema
+    var selectCinema = document.getElementById("searchCinema").value;
+    if ( selectCinema == "Omena") {
+        xmlhttp.open("GET", "https://www.finnkino.fi/xml/Schedule/?area=1039&dt=25.11.2020", true);
+        xmlhttp.send();
+    } else if (selectCinema == "Sello") {
+        xmlhttp.open("GET", "https://www.finnkino.fi/xml/Schedule/?area=1038&dt=25.11.2020", true);
+        xmlhttp.send();
+    } else if (selectCinema == "Itis") {
+        xmlhttp.open("GET", "https://www.finnkino.fi/xml/Schedule/?area=1045&dt=25.11.2020", true);
+        xmlhttp.send();
+    } else if (selectCinema == "Kinopalatsi") {
+        xmlhttp.open("GET", "https://www.finnkino.fi/xml/Schedule/?area=1031&dt=25.11.2020", true);
+        xmlhttp.send();
+    } else if (selectCinema == "Maxim") {
+        xmlhttp.open("GET", "https://www.finnkino.fi/xml/Schedule/?area=1032&dt=25.11.2020", true);
+        xmlhttp.send();
+    } else if (selectCinema == "Tennispalatsi") {
+        xmlhttp.open("GET", "https://www.finnkino.fi/xml/Schedule/?area=1033&dt=25.11.2020", true);
+        xmlhttp.send();
+    } else if (selectCinema == "Flamingo") {
+        xmlhttp.open("GET", "https://www.finnkino.fi/xml/Schedule/?area=1013&dt=25.11.2020", true);
+        xmlhttp.send();
+    } 
+    
+  }
+  function myFunction(xml) {
+    var i;
+    var xmlDoc = xml.responseXML;
+    var movieTitle = xmlDoc.getElementsByTagName("Show");
+    table = "<tr><th>Movie Title</th><th>Poster</th><th>Genre</th><th>Language and Display</th></tr>";
+    for (i = 0; i <movieTitle.length; i++) { 
+        table += "<tr><td>" + movieTitle[i].getElementsByTagName("Title")[0].childNodes[0].nodeValue + "</td><td>" + movieTitle[i].getElementsByTagName("RatingImageUrl")[0].childNodes[0].nodeValue + "</td><td>" + movieTitle[i].getElementsByTagName("Genres")[0].childNodes[0].nodeValue + "</td><td>" + movieTitle[i].getElementsByTagName("PresentationMethodAndLanguage")[0].childNodes[0].nodeValue + "</td></tr>";
+                   
+    }
+    document.getElementById("schedule").innerHTML = table;
+  }
